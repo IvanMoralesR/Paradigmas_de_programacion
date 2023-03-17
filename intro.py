1001533f67b610864ade459e3e4ccb38831c71b7
IvanMoralesR <ivsaimortius@gmail.com>
2023-03-17 12:44:49 -0600

    Practica 1

---
+++ intro.py
@@ -0,0 +1,53 @@
+'''
+COMENTARIO ACA LARGO, PARA ESO SIRVEN
+lAS 3 COMILLAS
+'''
+#####################
+#OPERACIONES BASICAS#
+#####################
+print (2+3) #print imprime
+print (2*3)
+print (2/3)
+print (2**10) #n**m n elevado a la m potencia
+print(2**0.5) #raíz cuadrada
+print(10%2) #modulo o residuo
+print(10%0.1) #exclusivo de python
+
+##########################################
+#Para saber el tipo de objeto se usa type#
+##########################################
+t=0
+print(type(t)) #entero
+t=3.6
+print(type(t))
+t=True
+print(type(t))
+
+######################
+# Mensaje en pantalla#
+######################
+print("Este es un comando de python","Este es otro",t) #comas separan weas de python
+print('id:','1')
+print('First Name')
+print('Last Name','Jobs')
+print('vamos a sumar esto'+'con esto otro')
+
+###########################################
+#Continuar instrucción en variod renglones#
+###########################################
+if 100>99 and \
+        200<= 300 and\
+        True!=False:
+            print('Hello World')
+
+#######################################
+#Comandos diferentes en la misma liena#
+#######################################
+print("Hols "); print ("tu!!") # se considera mala practica
+
+##############################################
+#Usando préntesis redondos cuadrados o llaves#
+#se puede escribir en varios renglones       #
+##############################################
+
+
