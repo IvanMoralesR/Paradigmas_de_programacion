7c45ba2e53aae4be7eae8eb30f87f3bc03ad7626
IvanMoralesR <ivsaimortius@gamail.com>
2023-06-19 13:00:54 -0600

    ia

--- Funciones/frikinarbol.py
+++ Funciones/frikinarbol.py
@@ -1,10 +1,10 @@
 ########################################################################
 #Grficos tortugosos de una tortuga que pinta tortugosoamente al caminar
 ########################################################################
-import turtle as turt
-tortuga = tort.Turtle()
-tortuga.left.(90)  #Giro a la izquierda con drift de 90 grados
-tortuga.speed(500) #Velocidad de la tortuga
+import turtle
+tortuga = turtle.Turtle()
+tortuga.left(90)  #Giro a la izquierda con drift de 90 grados
+tortuga.speed(400) #Velocidad de la tortuga
 
 ##################################
 #con angulos de 90 es un árbol H
@@ -15,6 +15,18 @@ angulo:float=90
 #El árbol se construye con recursividad
 ########################################
 def arbol(i:float,angulo:float):
-    if<10.0:
+    if i<10.0:
         return
+    else:
+        tortuga.forward(i) #camina i
+        tortuga.left(angulo) # gira a la izquierda 90 grados
+        arbol(3.0*i/4.25,angulo) #pide otro arbol y cambia la longitud de paso
+        tortuga.right(2*angulo) # gira a la derecha 180
+        arbol(3.0*i/4.25,angulo)
+        tortuga.left(angulo)
+        tortuga.backward(i)
+
+arbol(100,angulo)
+turtle.done()
+
 
