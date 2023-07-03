99be421ee709fe1af2e4843f6ef6affa3cb7f758
IvanMoralesR <ivsaimortius@gmail.com>
2023-07-03 11:56:48 -0600

    programaXZ

--- Paralelo/Numba/GPU/calcularpi.py
+++ Paralelo/Numba/GPU/calcularpi.py
@@ -8,7 +8,7 @@ import numpy as np
 import random
 
 @cuda.jit
-def calcularpi_kernel(rng_states, iteraciones, out):
+def calcularpi_kernel(rng_states, iteraciones, out_d):
     ii = cuda.grid(1)
     cae_adentro = 0
 
@@ -19,12 +19,12 @@ def calcularpi_kernel(rng_states, iteraciones, out):
         if x**2 + y**2 <= 1.0:
             cae_adentro += 1
 
-    out[ii] = 4.0 * cae_adentro / iteraciones
+    out_d[ii] = 4.0 * cae_adentro / iteraciones
 
 #weas de cuda 
 
 hilosporbloque = 64
-bloques = 24
+bloques = 256
 
 seed1 = random.uniform(-1, 1)
 seed2 = random.seed(seed1)
@@ -32,6 +32,8 @@ seed = random.randint(0, 1000)
 
 rng_states = create_xoroshiro128p_states(hilosporbloque * bloques, seed)
 out = np.zeros(hilosporbloque * bloques, dtype = np.float64)
+out_d = cuda.to_device(out)
 
-calcularpi_kernel[bloques, hilosporbloque](rng_states, 10000, out)
-print('pi: ', out_mean())
+calcularpi_kernel[bloques, hilosporbloque](rng_states, 10000, out_d)
+out_d.copy_to_host(out)
+print('pi: ', out.mean())
